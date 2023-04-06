99c7f7264ef546087dc77ad299b9c382d521bc1c
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2023-04-06 13:50:17 -0700

    refactor: working now with new json response from app.py (#1)

--- faucet/web/index.js
+++ faucet/web/index.js
@@ -1,30 +1,27 @@
 function transfer(address) {
-    fetch(`http://127.0.0.1:8000/transfer/${address}`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          console.log(`Error: ${data.error}`);
-        } else {
-            const responseDiv = document.getElementById('faucet-response');
-            const transactionData = data.transaction_data.split(' ');
-            let transactionOutput = '';
-            transactionData.forEach((item) => {
-                const keyValue = item.split('=');
-                const key = keyValue[0];
-                const value = keyValue[1];
-                transactionOutput += `"${key}": ${value}<br>`;
-            });
-            responseDiv.innerHTML = `<pre>${transactionOutput}</pre>`;
-        }
-      })
-      .catch(error => {
-        console.log(`Error: ${error.message}`);
-      });
+  fetch(`http://127.0.0.1:8000/transfer/${address}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        console.log(`Error: ${data.error}`);
+      } else {
+        const responseDiv = document.getElementById('faucet-response');
+        responseDiv.innerHTML = `
+          <pre>
+            "txn_hash": ${data.txn_hash}
+            "balance": ${data.balance}
+          </pre>
+        `;
+      }
+    })
+    .catch(error => {
+      console.log(`Error: ${error.message}`);
+    });
 }
 
 function callFaucet() {
-    const addressInput = document.getElementById('faucet-request');
-    const address = addressInput.value;
+  const addressInput = document.getElementById('faucet-request');
+  const address = addressInput.value;
 
-    transfer(address);
+  transfer(address);
 }
