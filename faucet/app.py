f68f1568112c0650b13bd93d5f928c8a52b6db22
Dogestradamus <27985126+sabotagebeats@users.noreply.github.com>
2023-05-10 03:49:13 -0700

    feat: supports account json environment variable [SBK-53] (#4)
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- faucet/app.py
+++ faucet/app.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 from typing import Annotated
@@ -17,15 +18,19 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-FAUCET_LIMIT = int(os.environ.get("FAUCET_TRANSFER_LIMIT", 10**18))
-
-NETWORK_TRIPLE = os.environ.get("FAUCET_RPC_ADDRESS", "http://localhost:8545")
+if account_json := os.environ.get("ACCOUNT_JSON"):
+    path = Path(__file__).parent / "bot.json"
+    path.write_text(account_json)
 
 ACCOUNT = KeyfileAccount(keyfile_path=(Path(__file__).parent / "bot.json"))
 ACCOUNT.set_autosign(True, passphrase="anvil")  # NOTE: Bundle account keyfile when building
 
+FAUCET_LIMIT = int(os.environ.get("FAUCET_TRANSFER_LIMIT", 10**18))
 
-class FacuetResponse(BaseModel):
+NETWORK_TRIPLE = os.environ.get("FAUCET_RPC_ADDRESS", "http://localhost:8545")
+
+
+class FaucetResponse(BaseModel):
     txn_hash: str
     txn_url: AnyUrl | None = None
     balance: int
@@ -38,7 +43,7 @@ async def transfer(
     address: str,
     amount: Annotated[int, Query(gt=0, lt=FAUCET_LIMIT)] = FAUCET_LIMIT,
     gas_limit: Annotated[int, Query(gt=21_000, lt=50_000)] = 35_000,
-) -> FacuetResponse:
+) -> FaucetResponse:
     """
     Send `amount` ether to `address`, with `gas_limit` given to transaction.
 
@@ -47,7 +52,7 @@ async def transfer(
     with networks.parse_network_choice(NETWORK_TRIPLE) as provider:
         # NOTE: Do not wait for confirmation
         tx = ACCOUNT.transfer(address, amount, gas_limit=gas_limit, required_confirmations=None)
-        return FacuetResponse(
+        return FaucetResponse(
             txn_hash=tx.txn_hash,
             txn_url=(
                 provider.network.explorer.get_transaction_url(tx.txn_hash)
