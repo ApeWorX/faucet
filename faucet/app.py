e72e79c0f6b839b901ad18920d56f91b891d52bb
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2023-11-17 04:46:15 -0800

    feat: list json sppt, erc20tx impersonation works, todo: balances and transfer

--- faucet/app.py
+++ faucet/app.py
@@ -1,9 +1,10 @@
+import json
 import os
 from pathlib import Path
 from typing import Annotated, list
 
 import uvicorn
-from ape import accounts, chain, networks
+from ape import chain, networks
 from ape.api.accounts import ImpersonatedAccount
 from ape.exceptions import ApeException
 from ape.types import AddressType
@@ -69,31 +70,53 @@ async def transfer(
         )
 
 
+token_list_source_uri = "https://gateway.ipfs.io/ipns/tokens.uniswap.org"
+
+# Create an instance of TokenListManager and install the 1inch token list
+tlm = TokenListManager()
+tlm.install_tokenlist(token_list_source_uri)
+
+
 @app.get("/token-list/{chain_id}")
 def token_list(chain_id: int):
     """
     Gets list of tokens
     """
-
     # Get tokens (you can customize this with parameters if needed)
-    tokens = tlm.get_tokens(token_listname="1inch", chain_id=chain_id)
+    tokens = tlm.get_tokens(token_listname="Uniswap Labs Default", chain_id=chain_id)
+    # Convert the filter object to a list
+    tokens_ls = list(tokens)
+    # Convert the list to json and return
+    token_ls = [ls.dict() for ls in tokens_ls]
+    return json.dumps(token_ls)
 
-    # Convert the filter object to a list and return it
-    return list(tokens)
 
+@app.get("/transfer-token/{chain_id}/{impersonated_account}")
+def transfer_token(chain_id: int, impersonated_account: str):
+    """
+    transfers tokens
+    """
 
-@app.get("/token-transfer/{token_address}/{chain_id}")
-def token_transfer(token_address: AddressType, chain_id: int):
-    """impersonate account and token transfer from that account"""
-    chain.provider._make_request(
-        "anvil_impersonateAccount", ["0x38225DE2EDa59e37b4B452c904fe21c507bbE4fa"]
-    )
-    acct = ImpersonatedAccount(raw_address="0x38225DE2EDa59e37b4B452c904fe21c507bbE4fa")
-    return acct.balance
+    """ impersonate account and token transfer from that account"""
+    anvil_address = "http://127.0.0.1:8545" #TODO: update to environment variable
+    with networks.ethereum.mainnet.use_provider(anvil_address) as provider:
+        assert chain_id == provider.chain_id
+        response = chain.provider._make_request("anvil_impersonateAccount", [impersonated_account])
+        acct = ImpersonatedAccount(raw_address=impersonated_account)
+        return acct.balance
+    
+# @app.get("/token-transfer/{token_address}/{chain_id}")
+# def token_transfer(token_address: AddressType, chain_id: int):
+#     """impersonate account and token transfer from that account"""
+#     chain.provider._make_request(
+#         "anvil_impersonateAccount", ["0x38225DE2EDa59e37b4B452c904fe21c507bbE4fa"]
+#     )
+#     acct = ImpersonatedAccount(raw_address="0x38225DE2EDa59e37b4B452c904fe21c507bbE4fa")
+#     return acct.balance
 
-    # account = accounts["example.eth"]
-    # account.transfer()
-    # return "method not implemented"
+#     # account = accounts["example.eth"]
+#     # account.transfer()
+#     # return "method not implemented"
 
 
 @app.exception_handler(ApeException)
